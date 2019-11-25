e690746289556bab07949596efd21e4fc9adb734
Mitchell Falkow <mdfalkow@gmail.com>
2019-11-25 00:02:47 -0500

    documentation (2)

--- models.py
+++ models.py
@@ -101,87 +101,94 @@ class LinearModel(BaseModel):
         self.X = np.asfarray(np.column_stack(
             (np.ones_like(self.X.shape[0]), self.X)))
         self.r = r
-        self.w = self.__train_model(n_iter, debug)
 
-    def __train_model(self,
-                      n_iter: int,
-                      debug: bool):
+        self._debug = debug
+        self._n_iter = n_iter
+
+        self.w = self._train_model()
+
+    def _train_model(self):
         """
         Train model on provided data using Linear regression then pocket 
         perceptron learning algorithm for improvement. 
+
+        Returns
+        -------
+        numpy.ndarray
+            The weight vector resulting from training
         """
-        w_ = self.__calc_w_lin(r)
-        E_ = self.__calc_E_in(w_, r)
-        if debug:
-            print(f'{time()} -- t: 0 -- (w_, E_): {(w_, E_)}')
-        w = np.copy(self.w_)
+        w_ = self._calc_w_lin()
+        E_ = self._calc_E_in(w_)
+
+        @staticmethod
+        def debug(t):
+            print(f'{time()} | iter: {t} | (E_, w_): {E_, w_}')
+
+        w = np.copy(self.w)
         for t in range(n_iter):
             if debug:
-                print(f'{time()} -- t: {t + 1} -- (w_, E_): {(w_, E_)}')
+                debug(t)
             while True:
-                i = random.randint(0, len(self.X) - 1)  # pick random point
-                pred = self.__classify(self.X[i], w)
-                if pred != self.y[i]:
+                # pick random point
+                i = random.randint(0, len(self.X) - 1)
+                if self.classify(self.X[i], w) != self.y[i]:
                     # Update w and E_in
                     w += self.y[i] * self.X[i]
-                    E_in = self.__calc_E_in(w, r)
+                    E_in = self._calc_E_in(w)
                     if E_in < E_:
                         E_ = E_in
                         np.copyto(w_, w)
                     break
         if debug:
-            print(f'{time()} -- t: {n_iter} -- Best Results: (w_, E_): {(w_, E_)}')
+            debug(self._n_iter)
 
         return w_
 
-    def __calc_w_lin(self):
+    def _calc_w_lin(self):
         """
-        Get w_lin, the weight vector from linear regression
+        Calculate w_lin, the weight vector from linear regression
 
-        Parameters
-        ----------
-        r : float
-            Regularization coefficient
-
+        Returns
+        -------
+        numpy.ndarray
+            The weight vector from determined by linear regression.
         """
         n = self.X.shape[1]
         return (np.linalg.inv((self.X.T @ self.X) +
                               self.r * np.eye(n)) @ self.X.T) @ self.y
 
-    def __calc_E_in(self, w: np.ndarray, r: float):
+    def _calc_E_in(self, w: np.ndarray = None) -> float:
         """
         Calculate in-sample error for specified weight vector
 
         Parameters
         ----------
-        w : numpy.ndarray
-
-        """
-        pred = np.sign(self.X @ w)
-        pred[pred == 0] = 1
-        return np.sum(np.abs(pred - self.y)) + self.r * (w @ w)
-
-    def __classify(self, x, w):
-        """
-        Returns the classification of x using provided the vector w
+        w : numpy.ndarray, optional
+            Weight vector for calculating E_in
+            (Default: None, uses model's weight vector)
 
         Returns
         -------
-        int 
-             1 if `np.dot(x, w)` >= 0
-            -1 otherwise
+        float
+            In-sample error, E_in, for the provided weight vector.
         """
-        return 1 if np.sign(np.dot(x, w)) >= 0 else -1
+        w = w if w != None else self.w
+        pred = self.X @ w
+        pred[pred >= 0] = 1
+        pred[pred < 0] = -1
+        return np.sum(np.abs(pred - self.y)) + self.r * (w @ w)
 
-    @staticmethod
-    def classify(x):
+    def classify(self, x: np.ndarray, w: np.ndarray = None) -> int:
         """
-        Returns the classification of x using the weight vector w of this model. 
+        Classifies x using the weight vector w. 
 
         Parameters
         ----------
         x : numpy.ndarray
             Point to classify
+        w : numpy.ndarray, optional
+            Weight vector to use for classification
+            (Default: None, uses model's weight vector)
 
         Returns
         -------
@@ -189,7 +196,7 @@ class LinearModel(BaseModel):
              1 if `np.dot(x, w)` >= 0
             -1 otherwise
         """
-        return self.__classify(x, self.w)
+        return 1 if np.dot(x, w) >= 0 else -1
 
 
 class kNNModel(BaseModel):
@@ -228,16 +235,16 @@ class kNNModel(BaseModel):
         self.k = k
 
     @staticmethod
-    def __euclidean_distance(x1, x2) -> float:
+    def _euclidean_distance(x1, x2) -> float:
         """
         Returns distance between two points
 
         Parameters
         ----------
-            x1 : numpy.ndarray
-                First point
-            x2 : numpy.ndarray
-                Second point
+        x1 : numpy.ndarray
+            First point
+        x2 : numpy.ndarray
+            Second point
 
         Returns
         -------
@@ -256,7 +263,7 @@ class kNNModel(BaseModel):
             Point to classify
         k : int, optional
             Number of nearest neighbors to check 
-            (Default: None, model uses attribute k).
+            (Default: None, uses model's k attribute).
 
         Returns
         -------
